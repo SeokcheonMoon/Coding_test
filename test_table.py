1be13daf47dcd9929fcc4608073c81021502635c
SeokcheonMoon <my980519@gmail.com>
2024-06-11 01:45:07 +0900

    add something

--- test_table.py
+++ test_table.py
@@ -1,5 +1,14 @@
-def solution(x):
-    answer = 0
-    return answer
+s = "a234"
+result = False
+
+try :
+    if (len(s) == 4 or len(s) == 6) and  bool(list(map(int,s))) == True :
+        answer = True
+    else : 
+        answer = False
+except : 
+    answer = False
+
+
 
 print(answer)
