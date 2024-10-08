eac2b9ad06772e50b76dc5c21be2441d56136b8f
seokcheon <151099231+SeokcheonMoon@users.noreply.github.com>
2024-10-08 11:24:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,43 +1,13 @@
 def solution(s):
-    try:
-        s = s.replace("zero","0")
-    except:
-        pass
-    try:
-        s = s.replace("one","1")
-    except:
-        pass
-    try:
-        s = s.replace("two","2")
-    except:
-        pass
-    try:
-        s = s.replace("three","3")
-    except:
-        pass
-    try:
-        s = s.replace("four","4")
-    except:
-        pass
-    try:
-        s = s.replace("five","5")
-    except:
-        pass
-    try:
-        s = s.replace("six","6")
-    except:
-        pass
-    try:
-        s = s.replace("seven","7")
-    except:
-        pass
-    try:
-        s = s.replace("eight","8")
-    except:
-        pass
-    try:
-        s = s.replace("nine","9")
-    except:
-        pass
+    s = s.replace("zero","0")
+    s = s.replace("one","1")
+    s = s.replace("two","2")
+    s = s.replace("three","3")
+    s = s.replace("four","4")
+    s = s.replace("five","5")
+    s = s.replace("six","6")
+    s = s.replace("seven","7")
+    s = s.replace("eight","8")
+    s = s.replace("nine","9")
     answer = int(s)
     return answer
